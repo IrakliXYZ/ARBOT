fd22ac42be2019b543b4c7fd5dca75daed5b3c17
Irakli <39024518+IrakliXYZ@users.noreply.github.com>
2021-11-07 03:46:08 +0000

    Changed the way of storing the data in Tracker file. Still not done.

--- tracker.py
+++ tracker.py
@@ -6,23 +6,24 @@ import time, datetime, json
 # Import sibling modules
 import sourcer, builder
 
-# pairs, that have leverages, that have data (spread, profit, effective rate...) that have hourly data
-while True:
-    for pair in sourcer.available_FTX:
-        for leverage in sourcer.available_leverages:
-            # Get the data
-            data = builder.generate_list(pair, 10000, leverage)
 
-            # Add the data to the json file
-            with open('tracker.json', 'a') as f:
-                # Problem here: need to format correctly in multidimensional way
-                f.write(json.dumps({'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'pair': pair, 'leverage': leverage, 'data': data} ))
-                f.write(',\n')
-
-    # Wait for the next hour
-    time.sleep(10)
-
-# loop that runs every hour and adds the builder.generate_list(pair, amnt, lvrg) data to the json file
+# Open folder Tracker and create a csv file for the data
+def write_file(pair, leverage):
+    # Open the file
+    file = open("Tracker/" + pair + "/" + leverage + ".csv", "w")
+    # Write data headers to csv file
+    file.write("Time, Spread %, Funding Rate H, Funding Rate D, Profit D, Effective Rate D, Funding Rate W, Profit W, Effective Rate W, Funding Rate M, Profit M, Effective Rate M, Funding Rate Y, Profit Y, Effective Rate Y\n")
 
 
+pairs = ['BTC', 'ETH', 'SOL', 'BNB', 'FTT', 'MATIC', 'XRP', 'LTC', 'SUSHI', 'RAY', 'LINK', 'CRV', 'COMP', 'GRT', '1INCH']
+leverages = ['2x', '3x', '4x', '5x', '6x', '7x']
 
+
+for pair in pairs:
+    for leverage in leverages:
+        # Open folder Tracker and create a csv file for the data
+        def write_file(pair, leverage):
+            # Open the file
+            file = open("Tracker/" + pair + "/" + leverage + ".csv", "w")
+            # Write data to csv file
+            file.write(builder.data) # + "\n"

a756c03c777ec8f994a3c0f7cf9d6859fd79df0c
Irakli <39024518+IrakliXYZ@users.noreply.github.com>
2021-11-07 16:15:21 +0000

    Finished the tracker but why does it mix the data? there is a clear order, but tracker.py likes the anarchy.

--- tracker.py
+++ tracker.py
@@ -1,29 +1,42 @@
 # This file keeps the program running and collects the data
 
 # Import the necessary packages
-import time, datetime, json
+import time, datetime, json, csv
 
 # Import sibling modules
 import sourcer, builder
 
+# Variables and lists
+pairs = ['BTC', 'ETH', 'SOL', 'BNB', 'FTT', 'MATIC', 'XRP', 'LTC', 'SUSHI', 'RAY', 'LINK', 'CRV', 'COMP', 'GRT', '1INCH']
+leverages = [2, 3, 4, 5, 6, 7]
+field_names = ['date_time', 'spread', 'funding_rate_h', 'funding_rate_d', 'profit_d', 'effective_rate_d', 'funding_rate_w', 'profit_w', 'effective_rate_w', 'funding_rate_m', 'profit_m', 'effective_rate_m', 'funding_rate_y', 'profit_y', 'effective_rate_y']
+
 
 # Open folder Tracker and create a csv file for the data
-def write_file(pair, leverage):
+def make_file(pair, leverage):
     # Open the file
     file = open("Tracker/" + pair + "/" + leverage + ".csv", "w")
     # Write data headers to csv file
     file.write("Time, Spread %, Funding Rate H, Funding Rate D, Profit D, Effective Rate D, Funding Rate W, Profit W, Effective Rate W, Funding Rate M, Profit M, Effective Rate M, Funding Rate Y, Profit Y, Effective Rate Y\n")
 
 
-pairs = ['BTC', 'ETH', 'SOL', 'BNB', 'FTT', 'MATIC', 'XRP', 'LTC', 'SUSHI', 'RAY', 'LINK', 'CRV', 'COMP', 'GRT', '1INCH']
-leverages = ['2x', '3x', '4x', '5x', '6x', '7x']
+def write_file(pair, leverage):
+            print("Writing file for pair: " + pair + " and leverage: " + str(leverage))
+            # Open folder Tracker and corresponding pair and leverage
+            with open("Tracker/" + pair + "/" + str(leverage) + "x" + ".csv", 'a') as csvfile:
+                # Add data to a file
+                writer = csv.writer(csvfile)
+                writer.writerow(builder.generate_list(pair, leverage))
+
+while True:
+    # Loop through all pairs and leverages
+    for pair in pairs:
+        for leverage in leverages:
+            print(builder.generate_list(pair, leverage))
+            write_file(pair, leverage)
+    # Wait for 1 hour
+    time.sleep(3600)
 
 
-for pair in pairs:
-    for leverage in leverages:
-        # Open folder Tracker and create a csv file for the data
-        def write_file(pair, leverage):
-            # Open the file
-            file = open("Tracker/" + pair + "/" + leverage + ".csv", "w")
-            # Write data to csv file
-            file.write(builder.data) # + "\n"
+
+        
